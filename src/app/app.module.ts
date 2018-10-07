26c551dcdfa99af1fe132e0bcaf091cebea03a31
eli <elizabeth.guerrero@ecloudsolutions.mx>
2018-10-07 14:57:09 -0500

    Structure of the project 1. Why is this change neccesary? It is necessary because I need a solid foundation for my project. 2. How does it address the issue? the technologies for my project have already been defined. 3. What side effects does this change have? none.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
+import { FormsComponent } from './forms/forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
