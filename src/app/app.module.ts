d071656c07881ba65569f0f29419987123a64d89
Elizabeth Torres <eli_darko@hotmail.com>
2018-10-09 18:34:58 -0500

    Structure of the application. 1. Why is this change neccesary? Separates the application into components. 2. How does it address the issue? The code is much more readable and optimal. 3. What side effects does this change have? none.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule } from '@angular/forms';
+import { NgSelectModule, NG_SELECT_DEFAULT_CONFIG } from '@ng-select/ng-select';
 
 import { AppComponent } from './app.component';
 import { FormsComponent } from './forms/forms.component';
+import { NameFieldsComponent } from './name-fields/name-fields.component';
+import { FieldsComponent } from './fields/fields.component';
+import { RulesComponent } from './rules/rules.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormsComponent
+    FormsComponent,
+    NameFieldsComponent,
+    FieldsComponent,
+    RulesComponent
   ],
   imports: [
     BrowserModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    FormsModule,
+    NgSelectModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: NG_SELECT_DEFAULT_CONFIG,
+      useValue: {
+        notFoundText: 'No hay resultados'
+      }
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
